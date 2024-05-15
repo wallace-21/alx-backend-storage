91655946560dcf1297b72550e0345d24df60ebb8
wallace <exnesswallace@gmail.com>
2024-05-14 20:42:43 -0700

    Python function that lists all documents in a collection

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def list_all(mongo_collection):
+    """
+    Retrieve all documents from a MongoDB collection.
+
+    Args:
+        mongo_collection: pymongo collection object to retrieve documents from.
+
+    Returns:
+        A cursor object containing all documents from the collection if documents are found,
+        otherwise an empty list.
+    """
+    action = mongo_collection.find({})
+    if len(action) == 0:
+        return []
+    return action
