529aa4d4903501322d2889a7b157aeba748bf9c4
wallace <exnesswallace@gmail.com>
2024-08-01 00:55:51 +0200

    made it exec and added shebang

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+""" Providing logs stored in MongoDB """
+from pymongo import MongoClient
+
+
+def nginx_logs(mongo_collection):
+     """stats about Nginx logs stored in MongoDB:"""
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    print(f"{mongo_collection.estimated_document_count()} logs")
+    print("Methods:")
+    for i in methods:
+        count = mongo_collection.count_documents({'method': i})
+        print(f"\tmethod {i}: {count}")
+
+    print(f"{mongo_collection.count_documents({'path': '/status'})}" +
+          " status check")
+
+
+if __name__ == "__main__":
+    nginx = MongoClient(host='localhost', port=27017).logs.nginx
+    nginx_logs(nginx)
